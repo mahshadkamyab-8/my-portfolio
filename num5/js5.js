6e86e14f42cde2533967e5eae4e3684dfe3bb652
Mahshad <mahshadkamyab8@gmail.com>
2026-09-20 22:07:54 +0330

    update portfolio

--- num5/js5.js
+++ num5/js5.js
@@ -1,22 +1,27 @@
 const alert = document.querySelector('.alert');
 const button = document.querySelector('.btn');
 const close = document.querySelector('.alert-box');
+const message_btn = document.querySelector('#mbtn');
 console.log(close)
 
 button.onclick = () => {
     showAlertBox();
+    message_btn.classList.remove('hidden');
     timer = setTimeout(function () {
         closeAlertBox();
-    }, 4000);
+        message_btn.classList.add('hidden');
+    }, 3000);
 }
 
 close.onclick = () => {
     closeAlertBox();
+    message_btn.classList.add('hidden');
     clearTimeout(timer);
 }
 
 function showAlertBox() {
     alert.classList.remove('hidden');
+
 }
 function closeAlertBox() {
     alert.classList.add('hidden');
